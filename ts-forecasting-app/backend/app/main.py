bce89d95bdaeabf823db75e98690aa6bdc03a928
fareslamass <121693297+lamassfaresm@users.noreply.github.com>
2025-12-30 22:48:58 +0100

    Update main.py

--- ts-forecasting-app/backend/app/main.py
+++ ts-forecasting-app/backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://bouthaina-app-p6yy-3ahik82nv-bouthainas-projects-0629f098.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
